c0881ff03eb108ce7a4f260e8f82adf3273cb9ff
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2020-10-29 08:56:59 -0700

    Updates

---
+++ precache-manifest.3f928644207e00838a14ff1bdea2ac70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b69c0ddf932bdff76549eb56ec0df62",
+    "url": "/todoapp-react-statehook/index.html"
+  },
+  {
+    "revision": "db31580e79af37d14852",
+    "url": "/todoapp-react-statehook/static/css/main.45a49f22.chunk.css"
+  },
+  {
+    "revision": "91e1e3152ba0a57f9dfa",
+    "url": "/todoapp-react-statehook/static/js/2.128a15f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp-react-statehook/static/js/2.128a15f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db31580e79af37d14852",
+    "url": "/todoapp-react-statehook/static/js/main.7dc0bf69.chunk.js"
+  },
+  {
+    "revision": "bbf6b15d9192114c2c1e",
+    "url": "/todoapp-react-statehook/static/js/runtime-main.4b6c39b4.js"
+  },
+  {
+    "revision": "80d9fee341699ba8d54a330e14b23f30",
+    "url": "/todoapp-react-statehook/static/media/todo.80d9fee3.svg"
+  }
+]);
